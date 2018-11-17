d4b1c3d60c3486ce82d8cfc27a2626d528f0a436
Simon de Lang <simondelang@gmail.com>
2018-11-17 10:41:40 +0100

    Add Stryker and HTTP_STATUS

---
+++ stryker.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config){
+  config.set({
+		coverageAnalysis: "off",
+		htmlReporter: {
+			baseDir: 'docs/mutation'
+		},
+		jest: {
+			config: require('./jest.config.js'),
+			projectType: 'custom'
+		},
+		loglevel: "debug",
+		mutate: [
+			'src/**/*.ts',
+			'!src/**/*.spec.ts'
+		],
+		mutator: "typescript",
+		packageManager: "npm",
+		reporters: ["html", "progress"],
+		testRunner: "jest",
+		transpilers: [],
+		tsconfigFile: "tsconfig.json"
+  });
+}
